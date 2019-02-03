16e34d2fe71d88364bfd8cece09ec9fd7bed5cc2
unknown <nellysattari@gmail.com>
2019-02-04 10:45:45 +1100

    added app component

--- src/components/index.js
+++ src/components/index.js
@@ -3,10 +3,12 @@ import TitleComponent from './TitleComponent.vue';
 import Ques1Component from './Ques1Component.vue';
 import Ques2Component from './Ques2Component.vue';
 import StepComponent from './StepComponent.vue';
+import AppComponent from '../App.vue';
 
 Vue.component('TitleComponent',TitleComponent);
 Vue.component('Ques1Component',Ques1Component);
 Vue.component('Ques2Component',Ques2Component);
 Vue.component('StepComponent',StepComponent);
+Vue.component('AppComponent',AppComponent);
 
-export default{TitleComponent,Ques1Component,Ques2Component,StepComponent}
+export default{TitleComponent,Ques1Component,Ques2Component,StepComponent,AppComponent}
